4a1853858c32cbbcf41843a6a2bd12f001ef544d
Timothy Geary <tdgeary@gmail.com>
2017-11-16 17:43:22 -0500

    Fixes checkbox parent relationships and spacing

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@
 
 "use strict";
 
+/*A simple attempt to wait for the page to finish loading*/
 function ready(main) {
   if (document.readyState === 'complete') {
     return main();
@@ -10,10 +11,12 @@ function ready(main) {
   document.addEventListener('DOMContentLoaded', main, false);
 }
 
+/*Mimics the jQuery toggle method*/
 function toggle(elem) {
   elem.style.display = (elem.style.display == 'none') ? 'block' : 'none';
 }
 
+/*Populates the pop-up with the bookmark bar's bookmarks and folders*/
 function viewBookmarks() {
   chrome.bookmarks.getTree(function displayTree(tree) {
     tree = tree[0];
@@ -26,6 +29,7 @@ function viewBookmarks() {
     for (let node of bookmarksBar.children) {
       displayNodes(node);
     }
+<<<<<<< Updated upstream
   });
 }
 
@@ -105,6 +109,152 @@ function displayBookmark(node, parent) {
   parent.appendChild(bookmarkOption);
 }
 
+=======
+    makeCheckboxesInteractive();
+  });
+}
+
+/*Decides whether or not the folder is a bookmark or folder*/
+function displayNodes(node, parent) {
+  parent = parent || document.getElementById("bookmarks");
+
+  if (node.children) {
+    displayFolder(node, parent);
+  } else {
+    displayBookmark(node, parent);
+  }
+}
+
+/*Populates the popup with a folder from the given node*/
+function displayFolder(node, parent) {
+  let folder = document.createElement('label');
+  folder.classList.add('folder');
+
+  let folderInput = document.createElement('input');
+  folderInput.setAttribute('type', 'checkbox');
+  folderInput.classList.add('folder-checkbox');
+  folder.appendChild(folderInput);
+
+  let folderToggle = document.createElement('span');
+  folderToggle.classList.add('folder-toggle');
+  folderToggle.classList.add('accordion-toggle');
+  folderToggle.append('▶');
+  folder.appendChild(folderToggle);
+
+  // animates arrow and adds accordion functionality
+  folderToggle.onclick = function () {
+    if (folderToggle.classList.contains('rotated')) {
+      folderToggle.classList.remove('rotated');
+    } else {
+      folderToggle.classList.add('rotated');
+    }
+
+    toggle(contents);
+  }
+
+  let icon = document.createElement('img');
+  icon.setAttribute('src', 'icon.png');
+  icon.classList.add('icon');
+  folder.append(icon);
+
+  folder.append(node.title);
+  let folderContainer = document.createElement('div');
+  folderContainer.appendChild(folder);
+  parent.appendChild(folder);
+
+  // creates the div holding the folder's contents
+  let contents = document.createElement('div');
+  contents.classList.add('folder-contents');
+  contents.classList.add('accordion-content');
+  if (node.children.length === 0) {
+    let text = document.createElement('p');
+    text.append('empty');
+    contents.appendChild(text);
+  } else {
+    for (let child of node.children) {
+      displayNodes(child, contents);
+    }
+  }
+  parent.appendChild(contents);
+  contents.style.display = 'none';
+}
+
+/*Populates the popup with a bookmark from the given node*/
+function displayBookmark(node, parent) {
+  let bookmarkOption = document.createElement('label');
+  bookmarkOption.classList.add('bookmark');
+
+  let bookmarkInput = document.createElement('input');
+  bookmarkInput.setAttribute('type', 'checkbox');
+  bookmarkInput.classList.add('bookmark-checkbox');
+  bookmarkOption.appendChild(bookmarkInput);
+
+  let bookmark = document.createElement('a');
+  bookmark.setAttribute('href', node.url);
+
+  let icon = document.createElement('img');
+  icon.setAttribute('src', 'chrome://favicon/' + node.url);
+  icon.classList.add('icon');
+  bookmark.appendChild(icon);
+
+  bookmark.append(node.title);
+
+  bookmarkOption.appendChild(bookmark);
+  parent.appendChild(bookmarkOption);
+}
+
+/*Adds checkbox features to folder and bookmark checkboxes*/
+function makeCheckboxesInteractive() {
+  const folders = document.querySelectorAll('.folder > input');
+  const contents = document.getElementsByClassName('folder-contents');
+
+  // selecting a bookmark changes the parent's checked status
+  for (let input of document.querySelectorAll('.bookmark-checkbox')) {
+    input.onclick = function () {
+      updateAllFolderCheckboxes(folders, contents);
+    }
+  }
+  for (let i = 0; i < folders.length; i++) {
+    // selecting a folder toggles selecting all of it's children
+    folders[i].onclick = function () {
+      for (let input of contents[i].querySelectorAll('input')) {
+        input.checked = folders[i].checked;
+      }
+      updateAllFolderCheckboxes(folders, contents);
+    }
+  }
+}
+
+/*Finding a parent folder is hard, so update everyone instead*/
+function updateAllFolderCheckboxes(folders, contents) {
+  for (let i = folders.length-1; i >= 0; i--) {
+    // -- instead of ++ so children are updated before parents
+    let checkboxes = contents[i].querySelectorAll('input');
+    let checkedNum = contents[i].querySelectorAll('input:checked');
+    if (checkboxes.length > 0) {
+      if (checkedNum.length === checkboxes.length) {
+        folders[i].checked = true;
+        folders[i].indeterminate = false;
+      } else if (checkedNum.length > 0) {
+        folders[i].checked = false;
+        folders[i].indeterminate = true;
+      } else {
+        folders[i].checked = false;
+        folders[i].indeterminate = false;
+      }
+    }
+
+    let checkedBoxes = document.querySelectorAll('.bookmark input:checked');
+    if (checkedBoxes.length > 0) {
+      displayTabsNum(checkedBoxes.length);
+    } else {
+      clearTabsNum();
+    }
+  }
+}
+
+/*Adds a badge with the given number on it to the popup's icon*/
+>>>>>>> Stashed changes
 function displayTabsNum(num) {
   chrome.browserAction.setBadgeBackgroundColor({
     color: [190, 190, 190, 0]
@@ -114,14 +264,19 @@ function displayTabsNum(num) {
   });
 }
 
+/*Gets rid of any badge currently on the popup's icon*/
 function clearTabsNum() {
   chrome.browserAction.setBadgeText({
     text: ''
   });
 }
 
+/*Acts like the main method*/
 ready(function main() {
+<<<<<<< Updated upstream
   displayTabsNum(5); //TODO - remove (this is to appease the linter for now)
   clearTabsNum(); //this one too
+=======
+>>>>>>> Stashed changes
   viewBookmarks();
 });
